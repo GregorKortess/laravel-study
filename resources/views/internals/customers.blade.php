8eecc7ed94cf0e263fe94c478ca1c6035175c14d
Cortess <CortessHack@gmail.com>
2019-07-09 18:21:42 +0300

    Form validation

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -4,9 +4,29 @@
 
     <h1>Customers</h1>
 
+    <form action="customers" method="POST" class="pb-4">
+
+        <p>Name:</p>
+        <div class="input-group pb-2">
+            <input type="text" name="name" value="{{ old('name') }}">
+            <div> {{ $errors->first('name') }} </div>
+        </div>
+
+        <p>Email:</p>
+        <div class="input-group pb-2">
+            <input type="text" name="email" value="{{ old('email') }}">
+            <div>  {{ $errors->first('email') }} </div>
+        </div>
+
+        <button type="submit" class="btn btn-default">Add customer</button>
+
+        @csrf
+    </form>
+
+
     <ul>
-       @foreach ($customers as $customer)
-            <li>{{$customer->name}}</li>
+        @foreach ($customers as $customer)
+            <li>{{$customer->name}} <span class="text-muted"> ({{$customer->email}}) </span></li>
         @endforeach
     </ul>
 
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::view('about', 'about');
 
 
 Route::get('customers', 'CustomersController@list');
+Route::post('customers', 'CustomersController@store');
 
 
 
