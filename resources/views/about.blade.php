0a42c769a326fc2e44b8b1d51f5276098e515fae
Cortess <CortessHack@gmail.com>
2019-07-11 17:20:55 +0300

    Cleaning Up the views

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,8 @@
 @extends('layout')
 
+@section('title','About us')
+
+
 @section('content')
     <h1>About us</h1>
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>@yield('title','laravel')</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -12,22 +12,9 @@
 </head>
 <body>
 
-<ul class="nav">
-    <li class="nav-item">
-        <a class="nav-link active" href="/">Home</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="about">About us</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="contact">Contact us</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="customers">Customers list</a>
-    </li>
-</ul>
 
 <div class="container">
+    @include('nav')
     @yield('content')
 </div>
 
