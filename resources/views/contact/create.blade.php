bfd8e8a513640100b0fc10a9db5bf9667103af28
Cortess <CortessHack@gmail.com>
2019-07-20 23:26:21 +0300

    mail

---
+++ resources/views/contact/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layout')
+
+@section('title','Contact us')
+
+@section('content')
+
+    <form action="/contact" method="POST">
+        <div  class="form-group">
+            <label for="name">Name</label>
+            <input type="text" name="name" value="{{ old('name')}}" class="form-control">
+            <div> {{ $errors->first('name') }} </div>
+        </div>
+
+        <div class="form-group pb-2">
+            <label for="email">Email</label>
+            <input type="text" name="email" value="{{ old('email')}}" class="form-control">
+            <div>  {{ $errors->first('email') }} </div>
+        </div>
+
+        <div class="form-group pb-2">
+            <label for="message">Message</label>
+            <textarea name="message"  rows="8" class="form-control" >{{ old('message') }}</textarea>
+            <div>  {{ $errors->first('message') }} </div>
+        </div>
+
+        <button type="submit" class="btn btn-outline-primary">Send Message</button>
+
+        @csrf
+    </form>
+
+@endsection
+
