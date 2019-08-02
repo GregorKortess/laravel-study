a9938eb2fd9b63e3cece5998bad14915108e9889
Cortess <CortessHack@gmail.com>
2019-08-03 01:20:00 +0300

    Eager loading

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -22,7 +22,7 @@ class CustomersController extends Controller
 
     public function index()
     {
-        $customers = Customer::all();
+        $customers = Customer::with('company')->get();
 
         return view('customers.index', compact('customers'));
     }
