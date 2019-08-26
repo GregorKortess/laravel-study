c2986a1bc73563290d19961d8c1c554e2d95aa0d
Cortess <CortessHack@gmail.com>
2019-08-26 18:50:46 +0300

    Multiple images upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
+    <div class="flex-center position-ref full-height">
+        <upload></upload>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        <my-button text="My new text button" type="submit"></my-button>
+        <div class="container">
+            <div class="row">
+                @foreach($images as $image)
+                    <div class="col-2 mb-4">
+                        <a href="{{ $image->original }}"><img src="{{ $image->thumbnail }}" class="w-100"></a>
+
+                        <form action="/images/{{$image->id}}" method="POST">
+                            @method("DELETE")
+                            @csrf
+                            <button class="small btn btn-outline-dark mt-2">Delete</button>
+                        </form>
                     </div>
 
-                </div>
+                @endforeach
+
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,13 @@
 |
 */
 
-Route::view('/', 'home');
+Route::get('/', 'UploadsController@index');
+Route::delete('/images/{imageUpload}','UploadsController@destroy');
+
 Route::get('contact','ContactFormController@create')->name('contact.create');
 Route::post('contact','ContactFormController@store')->name('contact.store');
 Route::view('about', 'about');
 Route::resource('customers','CustomersController');
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
+
